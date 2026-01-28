f68827f9715506af6a572c870fe43a54c052c42a
Bourouail Issam <issambourouail33@gmail.com>
2026-01-28 04:13:45 +0100

    Learn and implement a simple event with parameters
    - Practiced creating a custom event with parameters in a UserControl
    - Raised the event after calculation is completed
    - Handled the event in the form to receive and display the result
    - Added basic numeric input validation

---
+++ 1-Events/1-SimpleEventWithPrameters/MyUserControl.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleEventWithPrameters
+{
+    public partial class MyUserControl : UserControl
+    {
+
+        // Define a custom event handler delegate with parameters
+        public event Action<double> OnCalculationComplete;
+        // Create a protected method to raise the event with a parameter
+        protected virtual void CalculationComplete(double Result)
+        {
+            Action<double> handler = OnCalculationComplete;
+            if (handler != null)
+            {
+                handler(Result); // Raise the event with the parameter
+            }
+        }
+
+        public MyUserControl()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            if(!this.ValidateChildren()) 
+                return;
+
+            double Result =  Convert.ToDouble(txtNumber1.Text) + Convert.ToDouble(txtNumber2.Text);
+            lblResult.Text = Result.ToString();
+
+            if (OnCalculationComplete != null)
+                // Raise the event with a parameter
+                CalculationComplete(Result);
+
+        }
+
+        private void txtNumber_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            TextBox txt = sender as TextBox;
+
+            // Always allow control keys (Backspace, Delete, etc.)
+            if (char.IsControl(e.KeyChar))
+                return;
+
+            // Allow digits
+            if (char.IsDigit(e.KeyChar))
+                return;
+
+            // Allow ONE dot, not as first char
+            if (e.KeyChar == '.' && !txt.Text.Contains(".") && txt.SelectionStart > 0)
+                return;
+
+            // Block anything else
+            e.Handled = true;
+        }
+
+        private void txtNumber_Validating(object sender, CancelEventArgs e)
+        {
+            TextBox txt = sender as TextBox;
+
+            if (string.IsNullOrEmpty(txt.Text.Trim()))
+            {
+                e.Cancel = true;
+                errorProvider1.SetError((TextBox)sender, "Field cannot be empty!");
+                return;
+            }
+            else
+                errorProvider1.SetError((TextBox)sender, null);
+        }
+    }
+}
---
+++ 1-Events/1-SimpleEventWithPrameters/frmCalac.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace SimpleEventWithPrameters
+{
+    partial class frmCalac
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.myUserControl1 = new SimpleEventWithPrameters.MyUserControl();
+            this.SuspendLayout();
+            // 
+            // myUserControl1
+            // 
+            this.myUserControl1.AutoValidate = System.Windows.Forms.AutoValidate.Disable;
+            this.myUserControl1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.myUserControl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.myUserControl1.Location = new System.Drawing.Point(13, 14);
+            this.myUserControl1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.myUserControl1.Name = "myUserControl1";
+            this.myUserControl1.Size = new System.Drawing.Size(525, 184);
+            this.myUserControl1.TabIndex = 0;
+            this.myUserControl1.OnCalculationComplete += new System.Action<double>(this.myUserControl1_OnCalculationComplete);
+            // 
+            // frmCalac
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange;
+            this.ClientSize = new System.Drawing.Size(551, 213);
+            this.Controls.Add(this.myUserControl1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "frmCalac";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "frmCalac";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MyUserControl myUserControl1;
+    }
+}
