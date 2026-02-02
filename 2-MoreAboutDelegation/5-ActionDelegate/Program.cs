51a13a3b5c3d4126f46c6705ee0a555770d17283
Bourouail Issam <issambourouail33@gmail.com>
2026-02-02 17:22:55 +0100

    feat: Add Predicate delegate example - Demonstrated Predicate usage with integer parameter - Showed practical use of Predicate for validation logic

--- 2-MoreAboutDelegation/5-ActionDelegate/Program.cs
+++ 2-MoreAboutDelegation/5-ActionDelegate/Program.cs
@@ -1,15 +1,38 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace _5_ActionDelegate
+class Program
 {
-    internal class Program
+    static void Main()
     {
-        static void Main(string[] args)
-        {
-        }
+        // Declare and initialize Action delegates with regular methods
+
+        Action parameterlessAction = ParameterlessMethod;
+        Action<int> actionWithIntParameter = ActionWithIntParameterMethod;
+        Action<string, int> actionWithMultipleParameters = ActionWithMultipleParametersMethod;
+
+        // Invoking the actions
+
+        parameterlessAction();
+        actionWithIntParameter(42);
+        actionWithMultipleParameters("Hello, World!", 100);
+        Console.ReadKey();
+
+    }
+
+    // Regular methods to be used with Action delegates
+
+    static void ParameterlessMethod()
+    {
+        Console.WriteLine("This is a parameterless action.");
+    }
+
+    static void ActionWithIntParameterMethod(int x)
+    {
+        Console.WriteLine($"Action with int parameter: {x}");
+    }
+
+    static void ActionWithMultipleParametersMethod(string str, int num)
+    {
+        Console.WriteLine($"Action with string and int parameters: {str}, {num}");
     }
 }
