84ef5eb505e4a15da6390c272d5609389b97a42c
Bourouail Issam <issambourouail33@gmail.com>
2026-02-02 17:12:00 +0100

    feat: Add Action delegate examples

---
+++ 2-MoreAboutDelegation/4-FuncDelegate/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+
+public class Program
+{
+    // Define a delegate type for squaring a number
+    delegate int SquareDelegate(int x);
+
+
+    //// Define a Func delegate for squaring a number
+    static Func<int, int> squareForFuncDelegate = SquareMethod;
+
+    // Define a method that squares a number
+    static int SquareMethod(int x) { return x * x; }
+
+    static void Main()
+    {
+        // ###############################################
+        // ############### Normal Delegate ###############
+        // ###############################################
+
+        // Create an instance of the SquareDelegate and associate it with the SquareMethod
+        SquareDelegate squareForNormalDelegate = new SquareDelegate(SquareMethod);
+
+        //SquareDelegate squareForNormalDelegate = new SquareDelegate(SquareMethod);
+
+        // Use the square delegate to square the number 5
+        int resultNormalDelegate = squareForNormalDelegate(5);
+
+        // Print the result
+        Console.WriteLine("The square of 5 is: " + resultNormalDelegate);
+
+
+
+
+        // ###############################################
+        // ############### Func Delegate ###############
+        // ###############################################
+
+        // Use the square Func to square the number 5
+        int resultFuncDelegate = squareForFuncDelegate(20);
+
+        // Print the result
+        Console.WriteLine("The square of 20 is: " + resultFuncDelegate);
+        Console.ReadKey();
+    }
+}
---
+++ 2-MoreAboutDelegation/5-ActionDelegate/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("5-ActionDelegate")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("5-ActionDelegate")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5d870e4c-54a1-41b0-a290-ebc16b2c7a77")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
