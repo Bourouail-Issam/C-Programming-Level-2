b46829dafddcaf0c5803b0f514b27afd81b4852d
Bourouail Issam <issambourouail33@gmail.com>
2026-02-08 12:10:04 +0100

    refactor: reorganize nullable examples and add XML serialization example

---
+++ 6-Serialization and Deserialization/1-XML_Serialization_Example/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace _1_XML_Serialization_Example
+{
+    // It is not necessary to add the [Serializable] attribute to the Person class
+    // to enable XML serialization/deserialization.
+    [Serializable] 
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public short Age { get; set; }
+
+        public Person(int ID,string Name,short Age)
+        {
+            this.Id = ID;
+            this.Name = Name;
+            this.Age = Age;
+        }
+        // Required by XmlSerializer to create the object during deserialization
+        public Person()
+        {
+        }   
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create an instance of the Person class
+            Person person = new Person(1,"Micheal",42);
+
+            // XML serialization
+            XmlSerializer serializer = new XmlSerializer(typeof(Person));
+            using (StreamWriter writer = new StreamWriter("person.xml"))
+            {
+                serializer.Serialize(writer, person);
+            }
+
+            // Deserialize the object back
+            using (StreamReader reader = new StreamReader("person.xml"))
+            {
+                Person Deserialize  = (Person)serializer.Deserialize(reader);
+
+                Console.WriteLine($"ID : {Deserialize.Id} \nName : {Deserialize.Name} \nAge : {Deserialize.Age}");
+            }
+        }
+    }
+}
