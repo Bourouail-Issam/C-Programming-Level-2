48145dc9667038b45b0da920d9e1225f91fff8f7
Bourouail Issam <issambourouail33@gmail.com>
2026-02-14 06:05:50 +0100

    Add Reflection examples in C#
    - Demonstrated use of typeof and Type class
    - Retrieved type metadata (Name, FullName, IsClass)
    - Listed public methods of System.String using Assembly and BindingFlags
    - Reflected custom class (MyClass) to inspect properties and methods
    - Used Activator to create instance dynamically
    - Invoked methods and accessed properties via reflection

---
+++ 7-Reflection in C#/1-Type_Class/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_Type_Class
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Type type = typeof(String);
+
+            Console.WriteLine("\nType Information : \n");
+            Console.WriteLine($"Name : {type.Name}");
+            Console.WriteLine($"Full Name : {type.FullName}");
+            Console.WriteLine($"Is Class: {type.IsClass}");
+        }
+    }
+}
